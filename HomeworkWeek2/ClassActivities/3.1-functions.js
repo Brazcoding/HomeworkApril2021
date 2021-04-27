4192ab459fa6f717d4ccc161e1911c060a2e909e
eric neil <neil.eric.0101@gmail.com>
2021-04-26 19:26:22 -0500

    Returns and short circuiting

---
+++ HomeworkWeek2/ClassActivities/3.1-functions.js
@@ -0,0 +1,17 @@
+function greet() {
+  console.log("Hello!");
+}
+
+function threeModTwo(){
+  console.log(3 % 2)
+}
+
+let age = 18;
+function checkDrive() {
+  if (age >= 16)
+  console.log(young) {
+    else {
+      console.log(older)
+    }
+  }
+}
